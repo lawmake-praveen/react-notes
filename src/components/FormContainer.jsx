cc78e7803a20042f85083712a56e5dbd049f5b2a
lawmake-praveen <113996180+lawmake-praveen@users.noreply.github.com>
2023-07-10 22:33:37 +0530

    Update FormContainer.jsx

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const FormContainer = ({
   title,
@@ -12,35 +13,50 @@ const FormContainer = ({
 }) => {
   return (
     <div className="form-container" onClick={() => setShowNewPost(false)}>
-      <form className="form" onSubmit={handleSubmit} onClick={formTouch}>
-        <h1>New Post</h1>
-        <input 
-          autoFocus
-          type="text"
-          className="title"
-          placeholder="Title * (25 Characters Max)"
-          maxLength={25}
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-        <textarea
-          cols="30"
-          rows="10"
-          placeholder="Notes *"
-          value={notes}
-          onChange={(e) => setNotes(e.target.value)}
-          required
-        ></textarea>
-        <div className="button-area">
-          <button type="submit" className="submit-btn">
-            Submit
-          </button>
-          <button type="button" className="cancel-btn" onClick={handleCancel}>
-            Cancel
-          </button>
-        </div>
-      </form>
+        <motion.form
+          className="form"
+          onSubmit={handleSubmit}
+          onClick={formTouch}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <h1>New Post</h1>
+          <motion.input
+            autoFocus
+            type="text"
+            className="title"
+            placeholder="Title * (25 Characters Max)"
+            maxLength={25}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+          />
+          <motion.textarea
+            cols="30"
+            rows="10"
+            placeholder="Notes *"
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+            required
+            initial={{ opacity: 0, y: 20}}
+            animate={{ opacity: 1, y: 0}}
+          ></motion.textarea>
+          <div className="button-area">
+            <motion.button type="submit" className="submit-btn"
+              initial={{ y: 20 }}
+              animate={{ y: 0 }}
+            >
+              Submit
+            </motion.button>
+            <motion.button type="button" className="cancel-btn" onClick={handleCancel}
+              initial={{ y: 20 }}
+              animate={{ y: 0 }}>
+              Cancel
+            </motion.button>
+          </div>
+        </motion.form>
     </div>
   );
 };
